10e53c666ac86268e60a8801fdc992aec60fdc0e
Muhammad Tayyab Sheikh (CS Tayyab) <cstayyab@gmail.com>
2019-02-23 15:53:07 +0500

    Initial Commit

---
+++ foxdesign.js
@@ -0,0 +1,24 @@
+var currentTabId = null;
+var gettingCurrent = null;
+
+function onExecuted(result) {
+	console.log(`Design Mode Toggleds`);
+}
+
+
+function onExecuteError(error) {
+	console.log(`Error: ${error}`);
+}
+
+
+function ba_clicked(tabInfo) {
+	console.log("Tab Info: ", tabInfo);
+	currentTabId = tabInfo.id;
+	var executing = browser.tabs.executeScript(
+		currentTabId, {
+			code: `if(document.designMode==='on'){console.log('Switching Off Design Mode'); document.designMode='off';}else{console.log('Switching On Design Mode'); document.designMode='on';}`
+		}
+	);
+	executing.then(onExecuted, onExecuteError);
+}
+browser.browserAction.onClicked.addListener(ba_clicked);
